a76ffc1bbcf21e3b7851eac3290e16bdb8b0249c
leomaiagoncalves <leomaia@usp.br>
2025-06-19 01:17:50 -0300

    Update jogador_16882202.c

--- jogadores/jogador_16882202.c
+++ jogadores/jogador_16882202.c
@@ -1,15 +1,12 @@
+#include "jogador_16882202.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "baralho.h"
-
-
 
 static int meu_id_global;
 static int num_cartas_atuais;
-static Carta minha_mao_global[6]; 
+static Carta minha_mao_global[6];
 static int manilha_global;
 static int minha_aposta_global;
-
 
 static void remover_carta_da_mao_interna(int index) {
     for (int i = index; i < num_cartas_atuais - 1; i++) {
@@ -18,26 +15,24 @@ static void remover_carta_da_mao_interna(int index) {
     num_cartas_atuais--;
 }
 
-
-const char* nome() {
-	return "jogador_16882202";
+const char* nome_16882202() {
+    return "jogador_16882202";
 }
 
-void iniciar(const int id, const int n_jogadores) {
-	meu_id_global = id;
+void iniciar_16882202(const int id, const int n_jogadores) {
+    meu_id_global = id;
 }
 
-void nova_rodada(const Carta carta_virada, const int n_cartas, Carta* mao) {
+void nova_rodada_16882202(int rodada, const Carta carta_virada, int n_cartas, Carta* mao) {
     num_cartas_atuais = n_cartas;
-    manilha_global = get_manilha(carta_virada); 
+    manilha_global = get_manilha(carta_virada);
 
     for (int i = 0; i < n_cartas; i++) {
         minha_mao_global[i] = mao[i];
     }
 }
 
-int apostar(const int* apostas) {
-	// Estratégia de aposta: contar o número de "cartas fortes".
+int apostar_16882202(const int* apostas) {
     int cartas_fortes = 0;
     for (int i = 0; i < num_cartas_atuais; i++) {
         if (get_forca(minha_mao_global[i], manilha_global) >= 7) {
@@ -45,14 +40,13 @@ int apostar(const int* apostas) {
         }
     }
     minha_aposta_global = cartas_fortes;
-	return minha_aposta_global;
+    return minha_aposta_global;
 }
 
-int jogar(const int rodada, const int n_jogadas, const Carta* mesa, const int* apostas, const int* vitorias) {
-    // 1. Achar a carta mais forte que já está na mesa
+int jogar_16882202(Carta* mesa, int n_cartas_na_mesa, int vitorias) {
     int forca_max_mesa = -1;
-    if (n_jogadas > 0) {
-        for (int i = 0; i < n_jogadas; i++) {
+    if (n_cartas_na_mesa > 0) {
+        for (int i = 0; i < n_cartas_na_mesa; i++) {
             int forca_atual = get_forca(mesa[i], manilha_global);
             if (forca_atual > forca_max_mesa) {
                 forca_max_mesa = forca_atual;
@@ -62,17 +56,14 @@ int jogar(const int rodada, const int n_jogadas, const Carta* mesa, const int* a
 
     int indice_escolhido = -1;
 
-    // 2. Decide a estratégia: ganhar ou perder?
-    if (vitorias[meu_id_global] < minha_aposta_global) {
-        // --- ESTRATÉGIA: TENTAR GANHAR ---
+    if (vitorias < minha_aposta_global) {
         int idx_menor_ganhadora = -1;
-        int forca_menor_ganhadora = 100; // Valor artificialmente alto
+        int forca_menor_ganhadora = 100;
 
-        // Encontra a carta mais fraca que ainda seja capaz de ganhar a mão
         for (int i = 0; i < num_cartas_atuais; i++) {
             int forca_minha_carta = get_forca(minha_mao_global[i], manilha_global);
-            if (forca_minha_carta > forca_max_mesa) { // Se a carta ganha...
-                if (forca_minha_carta < forca_menor_ganhadora) { // ...e é a mais fraca até agora
+            if (forca_minha_carta > forca_max_mesa) {
+                if (forca_minha_carta < forca_menor_ganhadora) {
                     idx_menor_ganhadora = i;
                     forca_menor_ganhadora = forca_minha_carta;
                 }
@@ -80,9 +71,8 @@ int jogar(const int rodada, const int n_jogadas, const Carta* mesa, const int* a
         }
 
         if (idx_menor_ganhadora != -1) {
-            indice_escolhido = idx_menor_ganhadora; // Joga a mais fraca que ganha
+            indice_escolhido = idx_menor_ganhadora;
         } else {
-            // Se não pode ganhar, joga a pior carta para descartar
             int idx_pior_carta = 0;
             int forca_pior_carta = get_forca(minha_mao_global[0], manilha_global);
             for(int i = 1; i < num_cartas_atuais; i++) {
@@ -94,15 +84,13 @@ int jogar(const int rodada, const int n_jogadas, const Carta* mesa, const int* a
             indice_escolhido = idx_pior_carta;
         }
     } else {
-        // --- ESTRATÉGIA: TENTAR PERDER ---
         int idx_maior_perdedora = -1;
-        int forca_maior_perdedora = -1; // Valor baixo
+        int forca_maior_perdedora = -1;
 
-        // Encontra a carta mais forte que ainda perde a mão
         for (int i = 0; i < num_cartas_atuais; i++) {
             int forca_minha_carta = get_forca(minha_mao_global[i], manilha_global);
-            if (forca_minha_carta < forca_max_mesa) { // Se a carta perde...
-                if (forca_minha_carta > forca_maior_perdedora) { // ...e é a mais forte que perde
+            if (forca_minha_carta < forca_max_mesa) {
+                if (forca_minha_carta > forca_maior_perdedora) {
                     idx_maior_perdedora = i;
                     forca_maior_perdedora = forca_minha_carta;
                 }
@@ -110,10 +98,8 @@ int jogar(const int rodada, const int n_jogadas, const Carta* mesa, const int* a
         }
         
         if (idx_maior_perdedora != -1) {
-            indice_escolhido = idx_maior_perdedora; // Joga a mais forte que perde
+            indice_escolhido = idx_maior_perdedora;
         } else {
-            // Se todas as cartas ganham, é forçado a vencer.
-            // Joga a mais fraca delas para "minimizar o dano".
             int idx_pior_carta = 0;
             int forca_pior_carta = get_forca(minha_mao_global[0], manilha_global);
             for(int i = 1; i < num_cartas_atuais; i++) {
