d3943d07abc9525cb8081fe6565bf031d9c4767d
leomaiagoncalves <leomaia@usp.br>
2025-06-20 00:57:21 -0300

    Update jogador_16882202.h

--- jogadores/jogador_16882202.h
+++ jogadores/jogador_16882202.h
@@ -1,11 +1,12 @@
 #ifndef JOGADOR_16882202_H
 #define JOGADOR_16882202_H
 
-#include "../baralho.h" 
-const char* nome_16882202();
+#include "../baralho.h"
+
+const char* nome_jogador_16882202();
 void iniciar_16882202(const int id, const int n_jogadores);
 void nova_rodada_16882202(int rodada, const Carta carta_virada, int n_cartas, const Carta* mao);
 int apostar_16882202(const int* apostas);
-int jogar_16882202(Carta* cartas_na_mesa, int n_cartas_na_mesa, int vitorias);
+int jogar_16882202(Carta* mesa, int n_cartas_na_mesa, int vitorias);
 
-#endif 
+#endif
